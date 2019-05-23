9896203021ec71549ec629c779e7884ed8d8fffb
Davidodari <davidkibzodari@gmail.com>
2019-05-23 11:06:38 +0300

    instructions to setting up a recycler view

--- app/src/main/java/com/dsc/jkuat/moviehub/MainActivity.java
+++ app/src/main/java/com/dsc/jkuat/moviehub/MainActivity.java
@@ -1,8 +1,8 @@
 package com.dsc.jkuat.moviehub;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        TODO 1. Add Recycler View Dependency to the app module build.gradle and sync project with gradle
+//        TODO 2. Add Recycler View to activity layout
+//        TODO 3. Create instance of recycler view in activity java class with a layout manager
+//        TODO 4. Create Recycler View Adapter and set it to the recycler view
+//        TODO 5. Create Layout for recycler view items
+//        TODO 6. Bind Data to adapter to display in recycler view
     }
 }
